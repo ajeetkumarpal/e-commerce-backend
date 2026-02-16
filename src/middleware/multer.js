d188655c1552d214f0881740342a3dd0c2806416
Ajeet kumar pal <ajeetkumarpal22@gmail.com>
2026-02-16 20:40:15 +0530

    update

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,19 +1,26 @@
-import express from "express";
-import multer from "multer";
+import fs from "fs";
 import path from "path";
+import multer from "multer";
+
+const uploadDir = "/tmp/uploads";
+
+// Folder exist nahi karta to create kar do
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "/tmp/uploads"), // Vercel/Render ke liye
+  destination: (req, file, cb) => cb(null, uploadDir),
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     const baseName = path.basename(file.originalname, ext);
-    const uniqueName = baseName + Date.now() + ext;
-    cb(null, uniqueName);
+    cb(null, baseName + Date.now() + ext);
   },
 });
 
 const upload = multer({ storage: storage });
 export default upload;
+
 
 
 // import express from "express";
